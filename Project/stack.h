7a0c309f936e52a82d96c57ac9d775641b6c977b
lIlAntonlIl <anton.adamchuk.kpi@gmail.com>
2021-03-10 08:48:39 +0200

    Added StackInt for integer variables

--- Project/stack.h
+++ Project/stack.h
@@ -48,7 +48,6 @@ struct stack
         return returnedElement;
     }
 
-
     bool empty()
     {
         bool isEmpty;
@@ -65,5 +64,75 @@ struct stack
         return massOfElements[massSize];
     }
 
+
+};
+
+
+struct stackInt
+{
+    int massSize=-1;
+    int *massOfElements;
+
+
+    void clear()
+    {
+        massSize=-1;
+        delete[] massOfElements;
+    }
+
+
+    void push(int addedElement)
+    {
+        massSize++;
+        int *renewedMassOfElements = new int[massSize];
+        for (int i=0; i<=massSize-1;i++)
+            {
+                 renewedMassOfElements[i]= massOfElements[i];
+            }
+        renewedMassOfElements[massSize]= addedElement;
+        if (massSize>0)
+            delete[] massOfElements;
+        massOfElements=renewedMassOfElements;
+    }
+
+
+    int pop()
+    {
+        int returnedElement=massOfElements[massSize];
+        massSize--;
+        if(massSize!=-1)
+        {
+            int *renewedMassOfElements = new int[massSize];
+            for (int i=0; i<=massSize;i++)
+            {
+                 renewedMassOfElements[i]= massOfElements[i];
+            }
+            delete[] massOfElements;
+            massOfElements=renewedMassOfElements;
+
+        }
+        else
+        {
+            clear();
+        }
+        return returnedElement;
+    }
+
+
+    bool empty()
+    {
+        bool isEmpty;
+        if (massSize==-1)
+            isEmpty=true;
+        else
+            isEmpty=false;
+        return isEmpty;
+    }
+
+
+    int top()
+    {
+        return massOfElements[massSize];
+    }
 
 };
